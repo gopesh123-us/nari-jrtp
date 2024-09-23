ab728ae7fc3eaa3ee0994d7d92647571b87fb8d8
NarsiMyTeaching <narsi.myteaching@gmail.com>
2024-09-22 22:48:45 -0400

    implemented the outbound communication to i am user management api

--- acma-usermgmt-service/src/main/java/com/acma/properties/models/UsersResponse.java
+++ acma-usermgmt-service/src/main/java/com/acma/properties/models/UsersResponse.java
@@ -1,16 +1,24 @@
 package com.acma.properties.models;
 
+import java.io.Serializable;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@JsonDeserialize
-public class UsersResponse {
+@Builder
+public class UsersResponse implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6465221571377234927L;
+	
+	@JsonProperty
 	private List<Users> usersList;
 }
--- acma-usermgmt-service/src/main/java/com/acma/properties/outbound/AcmaUsersOutboundApi.java
+++ acma-usermgmt-service/src/main/java/com/acma/properties/outbound/AcmaUsersOutboundApi.java
@@ -4,13 +4,15 @@
 package com.acma.properties.outbound;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -19,13 +21,15 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.acma.properties.models.Users;
-import com.acma.properties.models.UsersResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author narsi
- * @since Sept-17,2024 
+ * @since Sept-17,2024
  */
 
 @Component
@@ -33,49 +37,103 @@ import lombok.extern.slf4j.Slf4j;
 public class AcmaUsersOutboundApi {
 
 	private RestTemplate restTemplate;
-	
+
+	private ObjectMapper objectMapper;
+
 	@Value("${acma.iam.usersApi}")
 	private String acma_users_api_url;
-	
-	
-	public AcmaUsersOutboundApi(RestTemplate restTemplate) {
+
+	public AcmaUsersOutboundApi(RestTemplate restTemplate, ObjectMapper objectMapper) {
 		this.restTemplate = restTemplate;
+		this.objectMapper = objectMapper;
 	}
-	
-	public List<Users> getAllPropertyOwners(String accessToken) throws RestClientException, URISyntaxException{
-		log.info("IAM Users API {}",acma_users_api_url);
-		
-		List<Users> usersList = new ArrayList<>();
-		
+
+	/**
+	 * Returns all the property owners
+ 	 * @param accessToken
+	 * @return
+	 * @throws RestClientException
+	 * @throws URISyntaxException
+	 * @throws JsonMappingException
+	 * @throws JsonProcessingException
+	 */
+	public List<Users> getAllPropertyOwners(String accessToken)
+			throws RestClientException, URISyntaxException, JsonMappingException, JsonProcessingException {
+		log.info("IAM Users API {}", acma_users_api_url);
+
+		MultiValueMap<String, String> headersMap = new LinkedMultiValueMap<>();
+		headersMap.add("Authorization", "bearer " + accessToken);
+
+		HttpEntity<String> entity = new HttpEntity<>(headersMap);
+		try {
+			long start = System.currentTimeMillis();
+			
+//			ResponseEntity<List<Users>> responseEntity = restTemplate.exchange(acma_users_api_url, HttpMethod.GET,
+//					entity, new ParameterizedTypeReference<List<Users>>() {
+//					});
+//			log.info("API Response Code is {}", responseEntity.getStatusCode().value());
+//			if (HttpStatus.OK.value() == responseEntity.getStatusCode().value()) {
+//				List<Users> usersList = responseEntity.getBody();
+//				log.info("Count of the Users {}", usersList.size());
+//				long end = System.currentTimeMillis();
+//				log.info("Total Time taken in millis is "+(end-start));
+//				return Optional.ofNullable(usersList).get();
+//
+//			} else {
+//				throw new RuntimeException("HttpStatusCode " + responseEntity.getStatusCode().value());
+//			}
+			 
+			
+			ResponseEntity<?> responseEntity = restTemplate.exchange(acma_users_api_url, HttpMethod.GET, entity, Object.class);
+			log.info("API Response Code is {}", responseEntity.getStatusCode().value());
+			if (HttpStatus.OK.value() == responseEntity.getStatusCode().value()) {
+				List<Users> usersListResp = (List<Users>) responseEntity.getBody();
+				log.info("Count of the Users {}", usersListResp.size());
+				long end = System.currentTimeMillis();
+				log.info("Total Time taken in millis is "+(end-start));
+				return usersListResp;
+			} else {
+				throw new RuntimeException("HttpStatusCode " + responseEntity.getStatusCode().value());
+			}
+			
+			
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public List<Users> getAllAgents() {
+		return null;
+	}
+
+	public List<Users> getAllBrokers() {
+		return null;
+	}
+
+	public Users createUser(Users user, String accessToken) {
 		
 		MultiValueMap<String, String> headersMap = new LinkedMultiValueMap<>();
-		headersMap.add("Authorization", "bearer "+accessToken);
+	    headersMap.add("Authorization", "Bearer "+accessToken);
+	    headersMap.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
 		
-		HttpEntity<String> entity = new HttpEntity<>(headersMap);
+	    HttpEntity requestBody = new HttpEntity<>(user,headersMap);
+	    
+	    ResponseEntity<?> responseEntity = restTemplate.postForEntity(acma_users_api_url, requestBody, null);
+	    log.info("API Response Code is {}", responseEntity.getStatusCode().value());
+		if (HttpStatus.CREATED.value() == responseEntity.getStatusCode().value()) {
+			HttpHeaders responseHeaders = responseEntity.getHeaders();
+			if(responseHeaders.containsKey("Location")){
+				String locHeaderVal = responseHeaders.getFirst("Location");
+				log.info("location header value is "+locHeaderVal);
+				locHeaderVal =  locHeaderVal.replace(acma_users_api_url+"/", "");
+				log.info("User Id From location header is "+locHeaderVal);
+			}
+		}    
 		
-		ResponseEntity<UsersResponse> responseEntity = restTemplate.exchange(acma_users_api_url, HttpMethod.GET, entity, UsersResponse.class);
-		
-	   //ResponseEntity<List> responseEntity =	restTemplate.getForEntity(acma_users_api_url, List.class,headersMap);
-	   log.info("API Response Code is {}",responseEntity.getStatusCode().value());
-	 if(HttpStatus.OK.value() == responseEntity.getStatusCode().value()) {
-		 UsersResponse usersRespBody =  responseEntity.getBody();
-		 log.info("Count of the Users {}",usersRespBody.getUsersList().size());
-		 
-	 }
+	    
 		return null;
 	}
-	
-	public List<Users> getAllAgents(){
-		return null;
-	}
-	
-	public List<Users> getAllBrokers(){
-		return null;
-	}
-	
-	public Users createUser(Users user) {
-		
-		return null;
-	}
-	
+
 }
