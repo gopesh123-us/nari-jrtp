d05d0dd534f2665d55332dab7c125ad0db9e1b79
NarsiMyTeaching <narsi.myteaching@gmail.com>
2024-10-29 16:15:05 -0400

    configured the discovery client

--- acma-authn-authz-service/src/main/java/com/acma/properties/AcmaAuthnAuthzServiceApplication.java
+++ acma-authn-authz-service/src/main/java/com/acma/properties/AcmaAuthnAuthzServiceApplication.java
@@ -2,14 +2,21 @@ package com.acma.properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AcmaAuthnAuthzServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcmaAuthnAuthzServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	
 }
